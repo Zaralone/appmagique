a4feae458719e46c31ec2333aee41c0227ec8ccb
Julien Martinez <julien.martinez45@orange.fr>
2018-09-17 14:17:41 +0200

    ok

--- app/src/main/java/com/example/administrateur/mystackappmagique/MainActivity.java
+++ app/src/main/java/com/example/administrateur/mystackappmagique/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.administrateur.mystackappmagique;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -15,13 +11,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.example.administrateur.mystackappmagique.api.StackClient;
-import com.example.administrateur.mystackappmagique.api.StackServiceInterface;
-import com.example.administrateur.mystackappmagique.api.pojo.Questions;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -33,14 +22,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
