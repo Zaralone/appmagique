9ba3389e3b499e12667f74dc03edc5a836a28123
Julien Martinez <julien.martinez45@orange.fr>
2018-09-17 14:58:04 +0200

    Maj fragments

--- app/src/main/java/com/example/administrateur/mystackappmagique/MainActivity.java
+++ app/src/main/java/com/example/administrateur/mystackappmagique/MainActivity.java
@@ -50,28 +50,6 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        switch (id){
-        case R.id.action_settings:
-            changeFragment(new Settings());
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -85,7 +63,9 @@ public class MainActivity extends AppCompatActivity
             case R.id.nav_favoris :
                 changeFragment(new Favoris());
                 break;
-
+            case R.id.nav_settings:
+                changeFragment(new Settings());
+                break;
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
