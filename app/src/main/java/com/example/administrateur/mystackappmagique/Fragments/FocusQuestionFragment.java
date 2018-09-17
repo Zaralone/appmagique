65e203e15fe27dd54cec1b932ec183a5e5a3d854
MorganBezard <morgan.bezard@gmail.com>
2018-09-17 15:28:57 +0200

    modifs stack exchange v99999k

---
+++ app/src/main/java/com/example/administrateur/mystackappmagique/Fragments/FocusQuestionFragment.java
@@ -0,0 +1,34 @@
+package com.example.administrateur.mystackappmagique.Fragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.administrateur.mystackappmagique.R;
+
+
+public class FocusQuestionFragment extends Fragment {
+
+
+    public FocusQuestionFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_focus_question, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
