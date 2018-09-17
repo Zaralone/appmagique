34bdcaaf5d1d234660fa3565ceef6f96c9ae7c42
MorganBezard <morgan.bezard@gmail.com>
2018-09-17 13:53:03 +0200

    modifs stack exchange v2

--- app/src/main/java/com/example/administrateur/mystackappmagique/DernieresQuestionsFragment.java
+++ app/src/main/java/com/example/administrateur/mystackappmagique/DernieresQuestionsFragment.java
@@ -2,6 +2,8 @@ package com.example.administrateur.mystackappmagique;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -20,15 +22,16 @@ import retrofit2.Response;
 public class DernieresQuestionsFragment extends Fragment {
 
 
-    public DernieresQuestionsFragment() {
-
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_dernieres_questions, container, false);
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        final RecyclerView recyclerView = findViewById(R.id.recyclerview);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final RecyclerView recyclerView = view.findViewById(R.id.recyclerview);
 
         StackServiceInterface service =
                 StackClient.getClient().create(StackServiceInterface.class);
@@ -47,7 +50,5 @@ public class DernieresQuestionsFragment extends Fragment {
 
             }
         });
-        return inflater.inflate(R.layout.fragment_dernieres_questions, container, false);
     }
-
 }
